ece30f1e4f6bb5d23794562a2cf9facc27cef24f
tr-fi <gnourtf@gmail.com>
2020-04-11 01:09:31 +0700

    redirect

--- express/routes.js
+++ express/routes.js
@@ -5,6 +5,12 @@ const cms_data = require('../cms_data.json')
 router.get('/cms', function(req, res) {
     res.json(cms_data);
 });
+router.get('/cms2', function(req, res) {
+    res.json(cms_data);
+});
+router.get('/cms3', function(req, res) {
+    res.json(cms_data);
+});
 
 
 module.exports = router;
