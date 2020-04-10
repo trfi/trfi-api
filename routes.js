75618359261345b0a6393477a382fe0fc3a7167c
tr-fi <gnourtf@gmail.com>
2020-04-10 15:48:06 +0700

    first:

---
+++ routes.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+const cms_data = require('./cms_data.json')
+
+
+router.get('/api/cms', function(req, res) {
+    res.json(cms_data);
+});
+
+
+module.exports = router;
