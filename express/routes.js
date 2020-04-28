a7c82bfdb630ed7832d806a56af54003b7e435da
tr-fi <gnourtf@gmail.com>
2020-04-28 17:40:06 +0700

    fix

--- express/routes.js
+++ express/routes.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router();
 const cms_data = require('../data/cms_data.json')
 const {checkKey} = require('../controllers/BraveTool')
+//const log_info = require('../logs/info.log')
+const fs = require('fs');
 
 router.get('/api/cms', function(req, res) {
     res.json(cms_data);
@@ -10,5 +12,12 @@ router.get('/api/toolbrave/checkkey', function (req, res) {
   res.send('nothing here')
 })
 router.post('/api/toolbrave/checkkey', checkKey)
+
+router.get('/logs/info', function (req, res) {
+  fs.readFile('./logs/info.log', 'utf8', function(err, data) {
+    if (err) throw err
+    return res.send(`<pre>${data}</pre>`)
+  })
+})
 
 module.exports = router
