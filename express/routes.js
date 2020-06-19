6234247c6b88a4577b9a062e3750ae942a97112f
tr-fi <gnourtf@gmail.com>
2020-06-19 11:08:28 +0700

    products

--- express/routes.js
+++ express/routes.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const router = express.Router();
 const cms_data = require('../data/cms_data.json')
+const products = require('../data/products.json')
 const {checkKey} = require('./controllers/BraveTool')
 const fs = require('fs');
 
 router.get('/api/cms', function(req, res) {
     res.json(cms_data);
+})
+router.get('/api/products', function(req, res) {
+  res.json(products);
 })
 router.get('/api/toolbrave/checkkey', function (req, res) {
   res.send('nothing here')
