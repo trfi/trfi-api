1bf1f54466929e98dd90b1810f879974e432976c
tr-fi <gnourtf@gmail.com>
2020-09-13 19:42:56 +0700

    add check key bravevps

---
+++ express/controllers/BraveVps.js
@@ -0,0 +1,26 @@
+const keys = require('../../data/key-brave-vps.json')
+
+
+function check(key) {
+  for (key_ of keys) {
+    if (key_ === key) return true
+  }
+  return false
+}
+
+module.exports = {
+  async checkKey (req, res, next) {
+    try {
+      console.log(req.body);
+      const {key} = req.body
+      if (check(key)) res.send({ status: 'success' })
+      else res.status(403).send({status: 'error', msg: 'Invalid Key'})
+    }
+    catch (err) {
+      console.log(err)
+      res.status(400).send({
+        error: err
+      })
+    }
+  }
+}
--- express/routes.js
+++ express/routes.js
@@ -2,7 +2,8 @@ const express = require('express')
 const router = express.Router();
 const cms_data = require('../data/cms_data.json')
 const products = require('../data/products.json')
-const {checkKey} = require('./controllers/BraveTool')
+const {bravetool} = require('./controllers/BraveTool')
+const {bravevps} = require('./controllers/BraveVps')
 const fs = require('fs');
 
 router.get('/api/cms', function(req, res) {
@@ -11,10 +12,14 @@ router.get('/api/cms', function(req, res) {
 router.get('/api/products', function(req, res) {
   res.json(products);
 })
+router.get('/api/products', function(req, res) {
+  res.json(products);
+})
 router.get('/api/toolbrave/checkkey', function (req, res) {
   res.send('nothing here')
 })
-router.post('/api/toolbrave/checkkey', checkKey)
+router.post('/api/toolbrave/checkkey', bravetool)
+router.post('/api/bravevps/checkkey', bravevps)
 
 
 module.exports = router
