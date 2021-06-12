4fe22a4197c6f96be63d9ce44bbf33f8ee8014a1
tr.fi <gnourtf@gmail.com>
2021-06-12 13:08:23 +0700

    update socket timeout and reconnect

--- express/server.js
+++ express/server.js
@@ -18,18 +18,47 @@ app.use('/api/quizpoly/lms', quizlms);
 
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
-// Connect to DB
-mongoose.connect(process.env.DB_CONNECTION,
-  { useUnifiedTopology: true }, 
-  { useNewUrlParser: true },
-  { socketTimeoutMS: 60000},
-  { heartbeatFrequencyMS: 5000},
-  { serverSelectionTimeoutMS: 30000 },
-  { keepAlive: true},
-  { useCreateIndex: true }
-)
-.then(() => console.log('DB Connected!'))
-.catch((err) => console.log(err));
+function connect() {
+  mongoose.connect(process.env.DB_CONNECTION,
+    { useUnifiedTopology: true }, 
+    { useNewUrlParser: true },
+    { socketTimeoutMS: 86400000},
+    { maxTimeMS: 30000 },
+    { useCreateIndex: true },
+    { serverSelectionTimeoutMS: 10000 },
+  )
+  .catch((err) => console.log(err));
+}
+
+const db = mongoose.connection;
+
+db.on('connecting', () => {
+  console.info('Connecting to MongoDB...');
+});
+
+db.on('error', (error) => {
+  console.error(`MongoDB connection error: ${error}`);
+  mongoose.disconnect();
+});
+
+db.on('connected', () => {
+  console.info('Connected to MongoDB!');
+});
+
+db.once('open', () => {
+  console.info('MongoDB connection opened!');
+});
+
+db.on('reconnected', () => {
+  console.info('MongoDB reconnected!');
+});
+
+db.on('disconnected', () => {
+  console.error(`MongoDB disconnected! Reconnecting in ${reconnectTimeout / 1000}s...`);
+  setTimeout(() => connect(), reconnectTimeout);
+});
+
+connect();
 
 module.exports = app;
 module.exports.handler = serverless(app);
